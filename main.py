cf5d9436396c5b35d96a4e24fbebdb0c387fce4c
dv8silencer <dv8silencer+github@gmail.com>
2020-11-18 23:18:09 -0600

    add license to source file

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+Copyright 2020 dv8silencer
+
+This file is part of eth1voting.
+
+eth1voting is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+eth1voting is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with eth1voting; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA -->
+"""
+
 import requests
 import json
 import urllib.parse

0feb9fc4c0e750fec4494e894550f6c9a8d679c6
dv8silencer <dv8silencer+github@gmail.com>
2020-11-19 12:01:10 -0600

    add 0x

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ for item in votesThis.items():
     url = "http://{}:{}/".format(gethhost, gethport)
     payload = {
         "method": "eth_getBlockByHash",
-        "params": [item[0].blockHash, True],
+        "params": ["0x" + item[0].blockHash, True],
         "jsonrpc": "2.0",
         "id": 0,
     }
